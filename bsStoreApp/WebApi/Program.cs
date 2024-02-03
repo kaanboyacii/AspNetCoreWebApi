18336e2f80fafb50976ddad02c6918a2cc9b88de
Kaan.Boyaci <Kaan.Boyaci@KAANBOYACI>
2024-02-03 11:24:03 +0300

    Trying on another pc

--- bsStoreApp/WebApi/Program.cs
+++ bsStoreApp/WebApi/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
     .AddNewtonsoftJson();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
