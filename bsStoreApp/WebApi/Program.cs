c8b85afb126131897f20d1ec0bb1f542ee800b8f
kaanboyacii <kaanboyacibn@gmail.com>
2024-02-03 11:48:33 +0300

    Kaan Boyaci commiit

--- bsStoreApp/WebApi/Program.cs
+++ bsStoreApp/WebApi/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers()
     .AddNewtonsoftJson();
-//deneme
+//Services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
