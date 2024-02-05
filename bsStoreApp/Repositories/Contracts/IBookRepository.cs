ca64271b79049c9374bacd122ef9e9c5044fa17a
kaanboyacii <kaanboyacibn@gmail.com>
2024-02-05 14:45:17 +0300

    Book Repository

---
+++ bsStoreApp/Repositories/Contracts/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts
+{
+    public interface IBookRepository : IRepositoryBase<Book>
+    {
+        IQueryable<Book> GetAllBooks(bool trankChanges);
+        IQueryable<Book> GetAllBookById(int id, bool trankChanges);
+        void CreateOneBook(Book book);
+        void UpdateOneBook(Book book);
+        void DeleteOneBook(Book book);
+    }
+}
---
+++ bsStoreApp/Repositories/EFCore/BookRepository.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    {
+        public BookRepository(RepositoryContext context) : base(context)
+        {
+            
+        }
+
+        public void CreateOneBook(Book book) => Create(book);
+        public void DeleteOneBook(Book book) => Delete(book);
+
+        public IQueryable<Book> GetAllBookById(int id, bool trankChanges) =>
+            FindByCondition(b => b.Id.Equals(id), trankChanges);
+
+        public IQueryable<Book> GetAllBooks(bool trankChanges) =>
+            FindAll(trankChanges)
+            .OrderBy(b=>b.Id);
+
+        public void UpdateOneBook(Book book) => Update(book);
+}
+}
