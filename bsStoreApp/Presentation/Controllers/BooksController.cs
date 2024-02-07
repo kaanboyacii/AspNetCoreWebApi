8b96a5dcc4fa2360bfc5258c8096a5f73b811811
kaanboyacii <kaanboyacibn@gmail.com>
2024-02-07 10:33:10 +0300

    BookController & BookManager exceptions setup

--- bsStoreApp/Presentation/Controllers/BooksController.cs
+++ bsStoreApp/Presentation/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts;
@@ -23,90 +24,57 @@ namespace Presentation.Controllers
         [HttpGet]
         public IActionResult GetAllBooks()
         {
-            try
-            {
-                var books = _manager.BookService.GetAllBooks(false);
-                return Ok(books);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            var books = _manager.BookService.GetAllBooks(false);
+            return Ok(books);
+
         }
         [HttpGet("{id:int}")]
         public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
         {
             var book = _manager
-                .BookService
-                .GetOneBookById(id, false);
-            if (book == null) return NotFound();
-
+                  .BookService
+                  .GetOneBookById(id, false);
             return Ok(book);
         }
         [HttpPost]
         public IActionResult CreateOneBook([FromBody] Book book)
         {
-            try
-            {
-                if (book == null)
-                    return BadRequest(); //400
-                _manager.BookService.CreateOneBook(book);
-                return StatusCode(201, book);
-            }
-            catch (Exception ex) { return BadRequest(ex.Message); }
+            if (book == null)
+                return BadRequest(); //400
+            _manager.BookService.CreateOneBook(book);
+            return StatusCode(201, book);
         }
         [HttpPut("{id:int}")]
         public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
         {
-            try
-            {
-                if (book is null)
-                    return BadRequest();
-                _manager.BookService.UpdateOneBook(id, book, true);
-                return NoContent(); //204
-            }
-            catch (Exception ex)
-            {
 
-                throw new Exception(ex.Message);
-            }
+            if (book is null)
+                return BadRequest();
+            _manager.BookService.UpdateOneBook(id, book, true);
+            return NoContent(); //204
         }
         [HttpDelete("{id:int}")]
         public IActionResult DeleteOneBooks([FromRoute(Name = "id")] int id)
         {
-            try
-            {
-                _manager.BookService.DeleteOneBook(id, false);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
 
-                throw new Exception(ex.Message);
-            }
+            _manager.BookService.DeleteOneBook(id, false);
+            return NoContent();
 
         }
         [HttpPatch("{id:int}")]
         public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
         {
-            try
-            {
-                //check book exist?
-                var entity = _manager
-                    .BookService
-                    .GetOneBookById(id, true);
 
-                if (entity == null) return NotFound(); //404
+            //check book exist?
+            var entity = _manager
+                .BookService
+                .GetOneBookById(id, true);
 
-                bookPatch.ApplyTo(entity);
-                _manager.BookService.UpdateOneBook(id, entity, true);
+            bookPatch.ApplyTo(entity);
+            _manager.BookService.UpdateOneBook(id, entity, true);
 
-                return NoContent(); //204
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            return NoContent(); //204
+
 
         }
     }
--- bsStoreApp/Services/BookManager.cs
+++ bsStoreApp/Services/BookManager.cs
@@ -1,11 +1,8 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Repositories.Contracts;
 using Services.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Services
 {
@@ -31,10 +28,7 @@ namespace Services
             //check entity
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
-            {
-                string message = $"The book with id:{id} could not found.";
-                _logger.LogInfo(message);
-            }
+                throw new BookNotFoundException(id);
             _manager.Book.DeleteOneBook(entity);
             _manager.Save();
         }
@@ -46,7 +40,11 @@ namespace Services
 
         public Book GetOneBookById(int id, bool trackChanges)
         {
-            return _manager.Book.GetOneBookById(id,trackChanges);
+            var book = _manager.Book.GetOneBookById(id,trackChanges);
+            if (book == null)
+                throw new BookNotFoundException(id);
+            return book;
+
         }
 
         public void UpdateOneBook(int id, Book book, bool trackChanges)
@@ -54,12 +52,7 @@ namespace Services
             //check entity
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
-            {
-                string message = $"The book with id:{id} could not found.";
-                _logger.LogInfo(message);
-                throw new Exception($"Book with id:{id} could not found");
-            }
-
+                throw new BookNotFoundException(id);
             entity.Title = book.Title;
             entity.Price = book.Price;
             _manager.Book.Update(entity);
